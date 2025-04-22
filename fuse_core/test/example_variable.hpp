390107b275c4a2fd4d88382132efa2d44520e879
Stephen Williams <swilliams@locusrobotics.com>
2025-04-22 06:46:30 +0200

    Port fix for Issue 300 from ROS 1 into ROS 2 (#404)
    Handle prior on last stamp (#323)
    * Added unit test to illustrate the bug from Issue 300
    * Move where the input variables are updated to the motion model values to ensure that transactions that involve only a single timestamp are correctly handled

--- fuse_core/test/example_variable.hpp
+++ fuse_core/test/example_variable.hpp
@@ -57,7 +57,11 @@ public:
   {
   }
 
-  size_t size() const override
+  explicit ExampleVariable(const rclcpp::Time & stamp)
+  : fuse_core::Variable(fuse_core::uuid::generate("ExampleVariable", stamp)),
+    data_(0.0)
+  {
+  }size_t size() const override
   {
     return 1;
   }
@@ -69,8 +73,12 @@ public:
   {
     return &data_;
   }
-  void print(std::ostream& /*stream = std::cout*/) const override
+  void print(std::ostream& stream = std::cout) const override
   {
+    stream << type() << ":\n"
+           << "  uuid: " << uuid() << "\n"
+           << "  data:\n"
+           << "  - [0]: " << data()[0] << "\n";
   }
 
 #if CERES_SUPPORTS_MANIFOLDS
